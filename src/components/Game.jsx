6db05256cf5d550dced04889629b8b1d20876b1e
Deepak Verma <deepakv032001@gmail.com>
2021-08-28 22:15:24 +0530

    Fifth Change

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -40,17 +40,25 @@ const Game = () => {
         {/* Game Section */}
         <div className="gSection my-4">
           <div className="row g-0">
-            <div className="col-3">Time</div>
-            <div className="col-6 d-flex justify-content-center align-items-center">
-              <img className="gameImg img-thumbnail" src={gameImg} alt="Game Img" />
+            <div className="col-4 d-flex justify-content-center align-items-center">
+              <div className="clock d-flex justify-content-center align-items-center rounded-circle">
+                45
+              </div>
             </div>
-            <div className="col-3">Time</div>
+            <div className="col-4 d-flex justify-content-center align-items-center">
+              <img
+                className="gameImg img-thumbnail"
+                src={gameImg}
+                alt="Game Img"
+              />
+            </div>
+            <div className="col-4 "></div>
           </div>
         </div>
         {/* Game Section */}
 
         {/* Game Question Section */}
-        <div className="gQuestion">
+        <div className="gQuestion p-2 d-flex justify-content-center align-items-center flex-column">
           <div className="lifeLine down flex-row justify-content-between">
             <button className="btn bg-transparent">
               <img src={fandf} alt="50:50" />
@@ -64,6 +72,31 @@ const Game = () => {
             <button className="btn bg-transparent">
               <img src={stq} alt="stq" />
             </button>
+          </div>
+
+          <div className="questions my-3 d-flex justify-content-center align-items-center flex-column p-2">
+            <div className="question d-flex justify-content-center align-items-center flex-column border p-3 rounded-pill">
+              As per Asoka’s inscriptions, which among the following place was
+              declared tax free and proclaimed only 1/8th part as taxable?
+            </div>
+            <div className="options p-1 w-100">
+              <div className="row">
+                <div className="col-md-6 p-3 rounded-pill my-1 col-12 opt border">
+                  Kushinagar
+                </div>
+                <div className="col-md-6 p-3 rounded-pill my-1 col-12 opt border">
+                  Lumbini
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6 p-3 rounded-pill my-1 col-12 opt border">
+                  Kathmandu
+                </div>
+                <div className="col-md-6 p-3 rounded-pill my-1 col-12 opt border">
+                  Sarnath
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         {/* Game Question Section */}
