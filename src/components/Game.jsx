d46bdf7db058877102e45af3d53eec92929ea985
Deepak Verma <deepakv032001@gmail.com>
2021-08-28 17:32:39 +0530

    Fourth Change

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import fandf from '../image/50-50_2018-0.png';
-import ata from '../image/ATA_2018-0.png';
-import dd from '../image/DoubleDip.png';
-import stq from '../image/switchTheQuestion.png';
+import fandf from "../image/50-50_2018-0.png";
+import ata from "../image/ATA_2018-0.png";
+import dd from "../image/DoubleDip.png";
+import stq from "../image/switchTheQuestion.png";
 import logo from "../kbcLogo1.png";
-import gameImg from "../image/gameImg.png"
+import gameImg from "../image/gameImg.png";
 
 const Game = () => {
   return (
@@ -14,16 +14,24 @@ const Game = () => {
         <div className="gHeader d-flex justify-content-center align-items-center my-4">
           <span className="hr"></span>
           <div className="w-100 d-flex justify-content-between align-items-center">
-            <div className="imgLogo"><img src={logo} alt="logo" /></div>
-            <p>
-              ₹0
-            </p>
+            <div className="imgLogo">
+              <img src={logo} alt="logo" />
+            </div>
+            <p>₹0</p>
 
-            <div className="lifeLine d-flex flex-row justify-content-between">
-              <button className="btn bg-transparent" ><img src={fandf} alt="50:50" /></button> 
-              <button className="btn bg-transparent" ><img src={ata} alt="ata" /></button> 
-              <button className="btn bg-transparent" ><img src={dd} alt="dd" /></button> 
-              <button className="btn bg-transparent" ><img src={stq} alt="stq" /></button> 
+            <div className="lifeLine top flex-row justify-content-between">
+              <button className="btn bg-transparent">
+                <img src={fandf} alt="50:50" />
+              </button>
+              <button className="btn bg-transparent">
+                <img src={ata} alt="ata" />
+              </button>
+              <button className="btn bg-transparent">
+                <img src={dd} alt="dd" />
+              </button>
+              <button className="btn bg-transparent">
+                <img src={stq} alt="stq" />
+              </button>
             </div>
           </div>
         </div>
@@ -34,7 +42,7 @@ const Game = () => {
           <div className="row g-0">
             <div className="col-3">Time</div>
             <div className="col-6 d-flex justify-content-center align-items-center">
-              <img className="gameImg" src={gameImg} alt="Game Img" />
+              <img className="gameImg img-thumbnail" src={gameImg} alt="Game Img" />
             </div>
             <div className="col-3">Time</div>
           </div>
@@ -43,7 +51,20 @@ const Game = () => {
 
         {/* Game Question Section */}
         <div className="gQuestion">
-          <p>Game Question Section</p>
+          <div className="lifeLine down flex-row justify-content-between">
+            <button className="btn bg-transparent">
+              <img src={fandf} alt="50:50" />
+            </button>
+            <button className="btn bg-transparent">
+              <img src={ata} alt="ata" />
+            </button>
+            <button className="btn bg-transparent">
+              <img src={dd} alt="dd" />
+            </button>
+            <button className="btn bg-transparent">
+              <img src={stq} alt="stq" />
+            </button>
+          </div>
         </div>
         {/* Game Question Section */}
       </div>
